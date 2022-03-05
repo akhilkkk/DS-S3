258b24b40d168c90227cdbd3ed79a150bdd275c0
AKHIL-KRISHNAN <98377774+akhilkkk@users.noreply.github.com>
2022-03-06 01:54:02 +0530

    Update infix_postfix.c

--- expt_8/infix_postfix.c
+++ expt_8/infix_postfix.c
@@ -130,3 +130,19 @@ int main() {
 			printf("\n Postfix: %s \n",post);
 			return 0;
 }
+
+
+//OUTPUT
+Enter the Expression : A*B+C/D-2
+
+ Token: A        stack :         postfix : A 
+ Token: *        stack : *       postfix : A 
+ Token: B        stack : *       postfix : A B 
+ Token: +        stack : +       postfix : A B * 
+ Token: C        stack : +       postfix : A B * C 
+ Token: /        stack : + /     postfix : A B * C 
+ Token: D        stack : + /     postfix : A B * C D 
+ Token: -        stack : -       postfix : A B * C D / + 
+ Token: 2        stack : -       postfix : A B * C D / + 2 
+ Postfix: AB*CD/+2- 
+
